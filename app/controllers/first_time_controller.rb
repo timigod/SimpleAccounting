63756e8c066403b52625eab1517618f5ccffc083
Timi <timi@helloworld.ng>
2015-06-25 19:13:34 +0100

    adds implementation to create default categories.

--- app/controllers/first_time_controller.rb
+++ app/controllers/first_time_controller.rb
@@ -4,6 +4,7 @@ class FirstTimeController < ApplicationController
       redirect_to '/users/sign_in'
     end
     @user = User.new
+
   end
 
 
@@ -11,6 +12,7 @@ class FirstTimeController < ApplicationController
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
+        create_default_categories
         format.html { redirect_to new_user_session_path, notice: 'You can now sign in to use SimpleAccounting' }
         format.json { render :show, status: :created, location: @user }
       else
@@ -22,6 +24,17 @@ class FirstTimeController < ApplicationController
 
 
   private
+
+  def create_default_categories
+    if Category.where(:name => 'Debt Settlement').exists?
+      @cat_debt = Category.new(:name => 'Debt Settlement')
+      @cat_debt.save
+    end
+    if Category.where(:name => 'Credit Settlement').exists?
+      @cat_credit = Category.new(:name => 'Credit Settlement')
+      @cat_credit.save
+    end
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
