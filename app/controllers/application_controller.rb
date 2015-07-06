174687e60d9f2028179ba637728eed591ecc2f3b
Timi <timi@helloworld.ng>
2015-07-06 18:47:15 +0100

    fixes creates default category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
+  create_default_categories
+
   def after_sign_in_path_for(resource_or_scope)
     transactions_path
+  end
+
+
+  private
+
+  def create_default_categories
+    if Category.where(:name => 'Debt Settlement').exists?
+      @cat_debt = Category.new(:name => 'Debt Settlement')
+      @cat_debt.save
+    end
+    if Category.where(:name => 'Credit Settlement').exists?
+      @cat_credit = Category.new(:name => 'Credit Settlement')
+      @cat_credit.save
+    end
   end
 end
